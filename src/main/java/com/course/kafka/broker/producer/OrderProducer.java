4c7750d1c909dbcb81e1b1312966de3e0c385fd0
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-04-17 16:23:37 +0530

    producer record to add header to kafka msg.

--- src/main/java/com/course/kafka/broker/producer/OrderProducer.java
+++ src/main/java/com/course/kafka/broker/producer/OrderProducer.java
@@ -3,9 +3,14 @@ package com.course.kafka.broker.producer;
 import com.course.kafka.broker.message.OrderMessage;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -16,10 +21,17 @@ public class OrderProducer
 
     public void publish(OrderMessage message)
     {
-        kafkaTemplate.send("t.commodity.order", message.getOrderNumber(), message)
+        kafkaTemplate.send(buildProducerRecord(message))
                      .addCallback(result -> log.info("Message sent successfully, message: {}, result: {}", message, result),
                                   e -> log.error("Error publishing message: {}", message, e));
 
         log.info("Message publish request has been sent for message:{}", message);
+    }
+
+    private ProducerRecord<String, OrderMessage> buildProducerRecord(OrderMessage message)
+    {
+        int bonus = message.getOrderLocation().startsWith("A")? 25: 15;
+        List<Header> headers = Collections.singletonList(new RecordHeader("surpriseBonus", String.valueOf(bonus).getBytes()));
+        return new ProducerRecord<>("t.commodity.order", null, message.getOrderNumber(), message, headers);
     }
 }
